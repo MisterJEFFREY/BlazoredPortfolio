b7bc1dc461b1c590e09b590affa136375c9b54db
Sir_Jeffrey_The_Cosio <31899831+MisterJEFFREY@users.noreply.github.com>
2021-08-26 22:40:10 -0700

    -Fixed 404 error w/ color_themes.css -Applied changes to Main Layout & Navmenu so Light & Dark Themes -Tried to Use JS Interop to dynamically change html tag's background color dynamically but it refused to work as intended or I don't understand how it truely works. Oh well. ^Resorted to making the @Body div min-height to 95vh

--- Shared/MainLayout.cs
+++ Shared/MainLayout.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
 namespace BlazoredPortfolio.Shared {
     public partial class MainLayout {
         #region Fields
@@ -15,13 +17,19 @@ namespace BlazoredPortfolio.Shared {
         public void On_MainColorThemeChanged(string tsValue) {
             if (!String.IsNullOrEmpty(tsValue)) {
                 _mainColorTheme = tsValue;
+                MyMethod();
             }
+        }
+        //See if this works to change html background color
+        async Task MyMethod() {
+          await JSRuntime.InvokeAsync<string>("applyStyleForHtml",
+              new { attrib = "background-color", value = "var(--light-body-color)" });
         }
         #endregion (Methods)
 
 
-        #region Properties
-        [Parameter]
+            #region Properties
+            [Parameter]
         public string MainColorTheme { get => _mainColorTheme; set => On_MainColorThemeChanged(value); }
 
         #endregion (Properties)
