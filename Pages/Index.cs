c0cd4125f9c9d9610f55c8dd8799def95294e23e
Sir_Jeffrey_The_Cosio <31899831+MisterJEFFREY@users.noreply.github.com>
2021-08-28 19:32:57 -0700

    -Started working on Index.razor's styling -Realized that I need to pass value from mainlayout to other pages ^Attempted to use cascading parameter ^Crossing fingers it works when deployed online and locally ^TODO: NEED TO GET IT TO WORK WHEN APP IS LOADED!

--- Pages/Index.cs
+++ Pages/Index.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using BlazoredPortfolio.Shared;
 using Microsoft.AspNetCore.Components;
 
 namespace BlazoredPortfolio.Pages {
@@ -13,11 +14,19 @@ namespace BlazoredPortfolio.Pages {
 
 
         #region Methods
-
+        protected override void OnInitialized() {
+            test_recieved_string = Layout.MainColorTheme;
+        }
         #endregion (Methods)
 
 
         #region Properties
+        //TODO: CAN THIS RECEIVE VALUE FROM MAINLAYOUT?
+        [CascadingParameter]
+        public MainLayout Layout { get; set; }
+
+        public string test_recieved_string { get; set; }
+
 
         #endregion (Properties)
     }
