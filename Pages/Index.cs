ffb86c7e51f7e8d9c12f95901251dcf1df44fcfa
Sir_Jeffrey_The_Cosio <31899831+MisterJEFFREY@users.noreply.github.com>
2021-08-30 21:59:15 -0700

    -Added Box Shadowing to nav link's hover + nonactive & active + nohover states -Gave Landing Text section its proper 'card' color style -Gave Color Theme Button opposite shadows to contrast from navbar buttons -Gave All Nav links a forced padding atribute to stay mostly consistent with color theme button -Completed Landing Intro & Contact Push Sections -Started Site Alterations Section

--- Pages/Index.cs
+++ Pages/Index.cs
@@ -25,19 +25,41 @@ namespace BlazoredPortfolio.Pages {
 
 
         protected override void OnParametersSet() {
-            test_recieved_string = Layout.MainColorTheme;
+            Color_Theme_Recieved = Layout.MainColorTheme;
         }
 
+        public void ChangeIndexResumeSection() {
+            if(IndexResumeButtonText != "Show Resume Link") {
+                IndexResumeButtonText = "Show Resume Link";
+                return;
+            }else if (IndexResumeButtonText == "Show Resume Link") {
+                IndexResumeButtonText = "Hide Resume Link";
+                return;
+            }
+        }
+
+        public void ChangeSiteLogSection() {
+            if(SiteAlterationLogButtonText != "Show Site Logs") {
+                SiteAlterationLogButtonText = "Show Site Logs";
+                return;
+            } else if(SiteAlterationLogButtonText == "Show Site Logs") {
+                SiteAlterationLogButtonText = "Hide Site Logs";
+                return;
+            }
+        }
         #endregion (Methods)
 
 
         #region Properties
         //TODO: CAN THIS RECEIVE VALUE FROM MAINLAYOUT?
+        //UPDATE: YES IT CAN
         [CascadingParameter]
         public MainLayout Layout { get; set; }
+        public string Color_Theme_Recieved { get; set; }
 
-        public string test_recieved_string { get; set; }
 
+        public string SiteAlterationLogButtonText { get; set; } = "Show Site Logs";
+        public string IndexResumeButtonText { get; set; } = "Show Resume Link";
 
         #endregion (Properties)
     }
---
+++ Pages/index_components/CmpSiteLog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazoredPortfolio.Pages.index_components {
+    public partial class CmpSiteLog {
+        #region Fields
+
+        #endregion (Fields)
+
+
+        #region Methods
+
+        #endregion (Methods)
+
+
+        #region Properties
+
+        #endregion (Properties)
+    }
+}
