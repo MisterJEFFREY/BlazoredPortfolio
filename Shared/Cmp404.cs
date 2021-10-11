17ff224305287e670475d2000fcae2eb10eeac17
Sir_Jeffrey_The_Cosio <31899831+MisterJEFFREY@users.noreply.github.com>
2021-10-11 16:07:51 -0700

    -Fixed Custom 404 message's color theme functionality ^OnIniAsync task was causing the issue -Moved 404 message into component...

---
+++ Shared/Cmp404.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using BlazoredPortfolio.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazoredPortfolio.Shared {
+    public partial class Cmp404 {
+        #region Methods
+        //////IMPORTANT METHODS TO RECIEVE COLOR THEMES DYNAMICALLY
+        //protected override async Task OnInitializedAsync() {
+        //    OnParametersSet();
+        //}
+
+        ////THIS WILL RECIEVE THE LIGHT/DARK COLOR THEME VALUE
+        ////DYNAMICALLY AND WORKS W/OUT COOKIES.
+        protected override void OnParametersSet() {
+            Color_Theme_Recieved = Layout.MainColorTheme;
+            InvalidURL = NavManager.Uri.ToString();
+        }
+        //////////////////////////////////////////////////////////////
+        #endregion (Methods)
+
+
+        #region Properties
+        [CascadingParameter]
+        public MainLayout Layout { get; set; } 
+        public string Color_Theme_Recieved { get; set; }
+        public string InvalidURL { get; set; }
+
+        #endregion (Properties)
+    }
+}
